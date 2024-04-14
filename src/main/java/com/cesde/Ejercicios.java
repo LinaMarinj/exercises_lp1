44e88b82f7bbcc8caf380b90213cfd6b19a101f7
LinaMarinj <linamarin2011@gmail.com>
2024-04-14 18:19:05 -0500

    ejercicios resueltos

--- src/main/java/com/cesde/Ejercicios.java
+++ src/main/java/com/cesde/Ejercicios.java
@@ -1,31 +1,54 @@
 package com.cesde;
 
 public class Ejercicios {
-    
+
     public String ejercicio1(int num1, int num2) {
-        // Implementar solución
-        return "";
-    }
-    
-    public String ejercicio2(double calificacion) {
-       // Implementar solución
-        return "";
+        if (num1 > num2) {
+            return "El número mayor es: " + num1;
+        } else if (num2 > num1) {
+            return "El número mayor es: " + num2;
+
+        } else {
+            return "Los dos números son iguales.";
+        }
     }
 
-    
-    public double ejercicio3(double precio) {
-        // Implementar solución
-        return 0;
+    public String ejercicio2(double calificacion) {
+        return (calificacion >= 3.5) ? "¡Felicidades! Usted ha aprobado el examen."
+                : "Lo siento, usted ha reprobado el examen.";
     }
-    
+
+    public double ejercicio3(double precio) {
+        if (precio >= 50000) {
+            double descuento = precio * 0.10;
+            return precio - descuento;
+        } else {
+            return precio;
+        }
+    }
+
     public String ejercicio4(int tipoFigura, double ladoCuadrado, double baseTriangulo, double alturaTriangulo,
             double radioCirculo) {
-        // Implementar solución
-        return "";
+        switch (tipoFigura) {
+            case 1:
+                return "El área de la figura es: " + ladoCuadrado * ladoCuadrado;
+
+            case 2:
+                return "El área de la figura es: " + baseTriangulo * alturaTriangulo / 2;
+            case 3:
+                return "El área de la figura es: " + Math.PI * Math.pow(radioCirculo, 2);
+
+            default:
+                return "Tipo de figura no válida.";
+        }
     }
-    
+
     public int ejercicio5(int num1, int num2, int num3) {
-        // Implementar solución
-        return 0;
+        if (num1 < num2 && num1 < num3) {
+            return num1;
+        } else if (num2 < num1 && num2 < num3) {
+            return num2;
+        }
+        return num3;
     }
 }
